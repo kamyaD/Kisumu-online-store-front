921895c6724586702faa95f5c9638bc5ad74c7bd
Kamya Domnick <domisemak@yahoo.co.uk>
2019-10-16 12:15:44 +0300

    Updates

---
+++ precache-manifest.a10502eefa9b1514fae430f0e2bb855d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e3943661aacf736a5618760e1b3a78",
+    "url": "/kisumuonline/index.html"
+  },
+  {
+    "revision": "d24dd783defc3e533f7a",
+    "url": "/kisumuonline/static/css/2.34a4b903.chunk.css"
+  },
+  {
+    "revision": "f3ddbbd685e9b715f3b2",
+    "url": "/kisumuonline/static/css/main.c9a060ca.chunk.css"
+  },
+  {
+    "revision": "d24dd783defc3e533f7a",
+    "url": "/kisumuonline/static/js/2.b8459886.chunk.js"
+  },
+  {
+    "revision": "f3ddbbd685e9b715f3b2",
+    "url": "/kisumuonline/static/js/main.0d709b7e.chunk.js"
+  },
+  {
+    "revision": "863612acbae106988896",
+    "url": "/kisumuonline/static/js/runtime-main.b8feee1d.js"
+  },
+  {
+    "revision": "8e40f20bc4f8f676edd141edd85ae612",
+    "url": "/kisumuonline/static/media/Sweat.8e40f20b.jpg"
+  },
+  {
+    "revision": "4ff6a5fa33a2aa5ed4e0e6b4dae8a976",
+    "url": "/kisumuonline/static/media/handbag.4ff6a5fa.jpg"
+  },
+  {
+    "revision": "725ba2eb89784b248df7223555bb572c",
+    "url": "/kisumuonline/static/media/shirt.725ba2eb.jpg"
+  },
+  {
+    "revision": "5d592e8a480cba7e9ab1218372b0b621",
+    "url": "/kisumuonline/static/media/shoe.5d592e8a.jpeg"
+  },
+  {
+    "revision": "39351e8572efb3c20ae2e32c57d1f2fb",
+    "url": "/kisumuonline/static/media/trouser.39351e85.jpg"
+  },
+  {
+    "revision": "a32b5fd3dbf2033735610ce4b6f6e87a",
+    "url": "/kisumuonline/static/media/womenshirt.a32b5fd3.jpg"
+  }
+]);
